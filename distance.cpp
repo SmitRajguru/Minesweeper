1ffef126e4ff257b9da990dea56fa4ae0348e8e7
Shubhangk <kulkar17@purdue.edu>
2016-07-07 18:39:20 +0530

    Update distance.cpp

--- distance.cpp
+++ distance.cpp
@@ -1,6 +1,10 @@
 #include<iostream.h>
 #include<distance.h>
 #include<math.h>
+double d(int x1,int y1, int x2, int y2)
+{
+	return max(abs(x1-x2),abs(y1-y2));
+}
 double gaussian(double a,double d)
 {
 	return exp(-1*pow((d/a),2));
