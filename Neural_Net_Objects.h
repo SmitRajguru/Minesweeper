0889983dd4ba334208608f6ee2fa9bd747ac65cf
anish-kulkarni <anish245@gmail.com>
2016-07-07 18:51:38 +0530

    Update Neural_Net_Objects.h

--- Neural_Net_Objects.h
+++ Neural_Net_Objects.h
@@ -7,6 +7,7 @@ class addGate
 {
     public:
     Unit x1,x2,y;
+    addGate();
     addGate(double, double);
     double forward();
     double backward();
@@ -16,7 +17,18 @@ class multiplyGate
 {
     public:
     Unit x1,x2,y;
+    multiplyGate();
     multiplyGate(double, double);
+    double forward();
+    double backward();
+}
+
+class distanceGate
+{
+    public:
+    Unit x,a,y;
+    distanceGate();
+    distanceGate(double);
     double forward();
     double backward();
 }
